681d3727f427e9c6862587cc738ff79a0fd2b64f
Chaykovskaya Daniela <cajkovskaadaniela@gmail.com>
2026-02-24 09:52:59 +0200

    App Router

---
+++ app/products/page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+    const users = [
+        { id: 1, name: 'Dana' },
+        { id: 2, name: 'Banana' }, ];
+    return (
+        <main>
+            <h1>Главная страница</h1>
+            <ul>
+                {users.map(user => (
+                    <p key={user.id}> {user.id}) {user.name}</p>
+                ))}
+            </ul>
+        </main>
+    );
+}
